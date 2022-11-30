2ec7c6ac0d79d792cdfe418242e87c079a9210e6
AaronManuel15 <aaronmanuel15@gmail.com>
2022-11-30 14:28:18 -0800

    Task 0

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Task 0: Write a Python script that, using this REST API,
+for a given employee ID, returns information about his/her
+TODO list progress.
+"""
+import requests
+from sys import argv
+
+
+def data_gather():
+    """makes two get requests to the api
+    """
+    url1 = 'https://jsonplaceholder.typicode.com/todos/?userId={}'
+    url2 = 'https://jsonplaceholder.typicode.com/users/{}'
+    todo_res = requests.get(url1.format(argv[1]))
+    user_res = requests.get(url2.format(argv[1]))
+
+    todo_res = todo_res.json()
+    user_name = user_res.json().get("name")
+    todo_length = len(todo_res)
+
+    todo_list = []
+
+    for item in todo_res:
+        if item.get('completed') is True:
+            todo_list.append(item.get("title"))
+
+    success_count = len(todo_list)
+
+    print('Employee {} is done with ({}/{})'
+          .format(user_name, success_count, todo_length))
+    for item in todo_list:
+        print('  {}'.format(item))
+
+
+if __name__ == "__main__":
+    data_gather()
